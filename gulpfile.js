1946972861aa098f05c11dbe75ff091f2650f410
Даниил Пронин <mail@grawl.ru>
2015-05-11 05:28:28 +1000

    chore(package) from `npm run` to `gulp`

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var harp = require('harp');
+var config = {
+	port: 8750,
+	public: {
+		dir: 'public/'
+	},
+	inject: '{sass,scss,less,styl}',
+	reload: '{jade,coffee,ejs,md,markdown}'
+};
+gulp.task('serve', function() {
+	config.public.files = config.public.dir + '/**/';
+	harp.server(config.public.dir, {
+		port: config.port
+	}, function() {
+		browserSync({
+			proxy: 'localhost:' + config.port,
+			port: config.port + 10,
+			open: false,
+			reloadOnRestart: true
+		});
+		gulp.watch([
+			config.public.files + '*.' + config.inject
+		], function() {
+			reload(
+				config.public.files + '*.css',
+				{stream: true}
+			);
+		});
+		gulp.watch([
+			config.public.files + '*.' + config.reload,
+			'*.' + config.reload
+		], function() {
+			reload();
+		});
+	})
+});
+gulp.task('default', ['serve']);
